4ca239a6b1fb84ac8adf26bbc4a243cd79739fe7
selvaggiamstrong316 <selvaggiamstrong316@gmail.com>
2025-03-07 09:31:54 +0000

    Add 2.js

---
+++ src/2.js
@@ -0,0 +1,14 @@
+const mathProblems = [
+  {
+    question: "What is the value of x in the equation 2x + 5 = 11?",
+    answer: 3
+  },
+  {
+    question: "Solve for y in the equation x + 5y = 20.",
+    answer: "y = 4"
+  },
+  {
+    question: "Find the value of c in the equation 3x - 5y + c = 20.",
+    answer: "c = 15"
+  }
+];
